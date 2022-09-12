bca425e1addacb2d63576fecf6e4af7f3c23233c
Abdulsemiu <oladonnie@hotmail.com>
2022-09-12 04:40:40 -0700

    sizeitup

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %i bytes(s)\n", sizeof(a));
-printf("Size of a int: %i bytes(s)\n", sizeof(b));
-printf("Size of a long int: %i bytes(s)\n", sizeof(c));
-printf("Size of a long long int: %i bytes(s)\n", sizeof(d));
-printf("Size of a float: %i bytes(s)\n", sizeof(e));
+printf("Size of a char: %i byte(s)\n", sizeof(a));
+printf("Size of a int: %i byte(s)\n", sizeof(b));
+printf("Size of a long int: %i byte(s)\n", sizeof(c));
+printf("Size of a long long int: %i byte(s)\n", sizeof(d));
+printf("Size of a float: %i byte(s)\n", sizeof(e));
 return (0);
 }
