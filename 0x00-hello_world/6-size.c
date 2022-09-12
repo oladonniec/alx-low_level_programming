faadf61b72c820b8891f07c996974e9a394771f2
Abdulsemiu <oladonnie@hotmail.com>
2022-09-12 04:18:58 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Printing out the memories allocated
+ *
+ * Reurn: compulsory 0
+ *
+ */
+int main(void)
+{
+char c;
+int b;
+long int c;
+long long int d;
+float e;
+
+printf("Size of a char: */id bytes(s)\n", sizeof(a));
+printf("Size of a int: */id bytes(s)", sizeof(b));
+printf("Size of a long int: */id bytes(s)", sizeof(c));
+printf("Size of a long long int: */id bytes(s)", sizeof(d));
+printf("Size of a float: */id bytes(s)", sizeof(e));
+return (0);
+}
