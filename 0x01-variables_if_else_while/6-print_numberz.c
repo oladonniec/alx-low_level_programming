0ef4aab4badbe0716f0cd7ff1d29c99dfa48df01
Abdulsemiu <oladonnie@hotmail.com>
2022-09-12 22:16:46 -0700

    comm

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Zero to nine
+ *
+ * Return: zero
+ *
+ */
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
