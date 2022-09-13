b86dbf5d823e9a386e0501f02f7115e53ca637d2
Abdulsemiu <oladonnie@hotmail.com>
2022-09-12 21:41:03 -0700

    comm

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Lowercase letters
+ *
+ * Return: should be 0
+ *
+ */
+int main(void)
+{
+char c;
+for (c='a';c<='z';c++)
+{
+putchar(c);
+putchar('\n');
+return (0);
+};
+}
