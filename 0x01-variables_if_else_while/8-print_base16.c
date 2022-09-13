75e9678f6289a48d13178e5985bf0f054fd79439
Abdulsemiu <oladonnie@hotmail.com>
2022-09-12 22:34:39 -0700

    commer

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Reverse alphabets
+ *
+ * Return: zero
+ *
+ */
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+for (c = 'a'; c <= 'y'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
