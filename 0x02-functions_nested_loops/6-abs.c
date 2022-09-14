5f585d2b41075b4ddeb0bb1a53c7adf6273ff62b
Abdulsemiu <oladonnie@hotmail.com>
2022-09-14 07:36:59 -0700

    functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/**
+* _abs - check absolute value of integer
+*
+* @c: int for argument
+*
+* Return: Always 0.
+*/
+int _abs(int c)
+{
+if (c > 0 || c == 0)
+{
+return (c);
+}
+else
+{
+return (c * -1);
+}
+}
