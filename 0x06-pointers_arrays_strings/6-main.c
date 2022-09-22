144682998b02acee88b3d10458f8185f1a59c246
Abdulsemiu <oladonnie@hotmail.com>
2022-09-22 10:12:32 -0700

    5to6

---
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include "6-cap_string.c"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char str[] = "Capitalize hello-world 0123456hello world\thello world.\n";
+char *ptr;
+ptr = cap_string(str);
+printf("%s", ptr);
+printf("%s", str);
+return (0);
+}
