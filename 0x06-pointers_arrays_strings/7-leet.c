17a26870b5d9a2cb6489bd0a3048ece40e1fd397
Abdulsemiu <oladonnie@hotmail.com>
2022-09-23 14:06:30 -0700

    seven

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,8 +10,9 @@ char *leet(char *s)
 int i, j;
 char a[] = "aAeEoOtTlL";
 char b[] = "4433007711";
-for (i = 0; *(s + i); i++){for (j = 0; j <= 9; j++)
-
+for (i = 0; *(s + i); i++)
+{
+for (j = 0; j <= 9; j++)
 {
 if (a[j] == *(s + i))
 *(s + i) = b[j];
